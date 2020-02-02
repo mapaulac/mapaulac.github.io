540726259d5ff2d91258c82987e0bf1711ecba07
María Paula Calderón <mpc441@nyu.edu>
2020-02-02 20:38:02 +0400

    changed fetch request url

--- dare/scriptb.js
+++ dare/scriptb.js
@@ -134,7 +134,7 @@ function callFetch() {
     } else if (choseDare3){
         fetchDare = finalDare3;
     }
-    fetch("http://localhost:5000/bsend", 
+    fetch("http://gameofdare.herokuapp.com/bsend", 
     {
         method: 'POST', // *GET, POST, PUT, DELETE, etc.
         //mode: 'cors', // no-cors, *cors, same-origin
@@ -160,7 +160,7 @@ function callFetch() {
 
 //OBTAIN FROM JSON
 function receiveFetch() {
-    fetch("http://localhost:5000/bsend", 
+    fetch("http://gameofdare.herokuapp.com/bsend", 
     {
         method: 'POST', // *GET, POST, PUT, DELETE, etc.
         //mode: 'cors', // no-cors, *cors, same-origin
